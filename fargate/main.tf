ae2891e8799ee2d6070438438cfef05dd2cfc089
DanyloSafankov <youremail@example.com>
2024-01-02 15:26:27 +0200

    EKS examples

---
+++ fargate/main.tf
@@ -0,0 +1,33 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source             = "github.com/terraform-aws-modules/terraform-aws-vpc"
+  name               = var.vpc_name
+  cidr               = var.cidr_block
+  azs                = data.aws_availability_zones.available.names
+  public_subnets     = [cidrsubnet(var.cidr_block, 8, 3), cidrsubnet(var.cidr_block, 8, 4), cidrsubnet(var.cidr_block, 8, 5)]
+  private_subnets    = [cidrsubnet(var.cidr_block, 8, 0), cidrsubnet(var.cidr_block, 8, 1), cidrsubnet(var.cidr_block, 8, 2)]
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+module "eks" {
+  source                         = "github.com/terraform-aws-modules/terraform-aws-eks"
+  cluster_name                   = "demo-cluster"
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  control_plane_subnet_ids       = module.vpc.public_subnets
+  create_kms_key                 = true
+  cluster_endpoint_public_access = true
+  fargate_profiles = [
+    {
+      name = "node-group-1"
+      selectors = [{
+        namespace = "default"
+      }]
+    },
+  ]
+}
+
